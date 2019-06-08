707d11842897032d20b06cbaf20e7a126e92200f
leemirim <leemirim9654@gmail.com>
2019-06-08 21:27:41 +0900

    first commit

---
+++ src/main/java/com/mimipjt/firstpjt/FirstpjtApplication.java
@@ -0,0 +1,13 @@
+package com.mimipjt.firstpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstpjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstpjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mimipjt/firstpjt/FirstpjtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mimipjt.firstpjt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstpjtApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
